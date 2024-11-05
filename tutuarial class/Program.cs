e2210d54170cb8ac0dc45a2bee09dbbe41fefb64
amir hossein naseh <jjamirjj@outlook.com>
2024-11-05 16:55:41 +0300

    1

--- tutuarial class/Program.cs
+++ tutuarial class/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//start
+
+namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
+
+/*
+Console.WriteLine("Please Enter Name");
+string name = Convert.ToString(Console.ReadLine()).ToUpper();
+Console.WriteLine("Please Enter Family");
+string family = Convert.ToString(Console.ReadLine()).ToUpper();
+
+string FullNAme = name + " " + family;
+
+Console.WriteLine();
+
+switch (FullNAme)
+{
+    case "REZA SOLTANI":
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello Reza Soltani, your login has been registered successfully");
+            break;
+        }
+    case "SAMA SOLTANI":
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello sama Soltani, your login has been registered npot");
+            break;
+        }
+    default:
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Your name is not registered in the system");
+            break;
+
+        }
+
+}
+*/
---
+++ tutuarial first/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("please enter a or b ");
+char input ;
+input = Console.ReadKey().KeyChar;
+
+if( input == 'a' || input == 'A' ) 
+{
+    if (input == 'r' || input == 'R')
+    {
+
+
+    }
+    else if ()
+    {
+
+    }
+    
+
+}
+else if (input == 'b' || input == 'B')
+{
+
+}
